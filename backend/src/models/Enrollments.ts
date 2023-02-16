75245ee844e7e85cf170f2b14f3224a3987e8564
unknown <ugasdomingo@gmail.com>
2023-02-16 16:41:42 +0100

    Add enrollment backend models

---
+++ backend/src/models/Enrollments.ts
@@ -0,0 +1,34 @@
+// Import tools
+import { Schema, model } from "mongoose";
+
+//Create Enrollment Schema
+const EnrollmentSchema = new Schema({
+    date: {
+		type: Date,
+		default: Date.now(),
+	},
+    formationID: {
+		type: String,
+		required: true,
+	},
+	numberProof: {
+		type: String,
+		required: true,
+	},
+    payed: {
+		type: Boolean,
+		required: true,
+		default: false,
+	},
+    paymentMethod: {
+		type: String,
+		required: true,
+	},
+    uid: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+})
+
+export default model("Enrollments", EnrollmentSchema);
